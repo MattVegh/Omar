91a287f96a64fb5621578afdc7d181d616b85569
Matt Vegh <mdv240@gmail.com>
2020-07-11 22:39:35 -0500

    sample data

---
+++ sampleData.js
@@ -0,0 +1,10 @@
+const columnArray = ['Name', 'Class', 'Level', 'Gold']
+
+const rowArray = [
+    ['Leo', 'warrior', 1, 41],
+    ['Ragnarok', 'warlock', 2, 450],
+    ['Denzel', 'cleric', 3, 120],
+    ['Grey', 'rogue', 4, 22]
+]
+
+export {columnArray, rowArray}
